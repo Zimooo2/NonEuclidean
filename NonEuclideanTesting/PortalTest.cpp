c28e6c44079abc481fd4934bee22c4bf0c14853a
crollt <crollt@oregonstate.edu>
2019-02-05 19:36:42 -0800

    TESTING

---
+++ NonEuclideanTesting/PortalTest.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../NonEuclidean/Portal.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NonEuclideanTesting
+{
+	TEST_CLASS(Portal_Test)
+	{
+	public:
+		TEST_METHOD(Draw_UnitTest)
+		{
+			Portal p;
+		}
+	};
+}
